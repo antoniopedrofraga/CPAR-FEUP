6e7addf6f3e977d05596f585773bbe23f32d20ba
Pedro Fraga <antoniopedrofraga@gmail.com>
2017-02-24 17:16:35 -0800

    python code is done

--- Matrix/python/matrix.py
+++ Matrix/python/matrix.py
@@ -1,3 +1,5 @@
+import time
+
 def showCli():
 	print("1. Multiplication")
 	print("2. Line Multiplication")
@@ -9,6 +11,30 @@ def showCli():
 	multiplyMatrices(lines, cols)
 
 def multiplyMatrices(lines, cols):
-	
+	phc = phb = pha = [[None for i in range(lines)] for j in range(lines)]
+	temp = None
+
+	for i in range(lines):
+		for j in range(lines):
+			pha[i][j] = float(1.0) #float is a C double
+
+	for i in range(cols):
+		for j in range(cols):
+			phb[i][j] = float(i + 1);
+
+	time1 = time.time()
+
+	for i in range(lines):
+		for j in range(cols):
+			temp = float(0)
+			for k in range(lines):
+				temp += pha[i][k] * phb[k][j]
+			phc[i][j] = temp
+
+	time2 = time.time()
+
+	print("Time: " + str(time2 - time1) + " seconds")
+
+	del pha, phb, phc
 
 showCli()
